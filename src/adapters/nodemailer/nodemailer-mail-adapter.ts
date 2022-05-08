126a5c657f7d7e6cba63aaf6102fc66edfc17df4
guisantanaa <santanagui40@gmail.com>
2022-05-08 10:43:41 -0300

    fist commit server impulse

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '207b260829dbd7',
+    pass: '68fe9e917491e8',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Guilherme Santana <santanagui40@gmail.com>',
+      subject: subject,
+      html: body,
+    });
+  }
+}
